85ad455d22b8d2e32de7384e81bdb605326d2f6c
Asriel <1134175939@qq.com>
2022-09-17 22:45:31 +0800

    Google Analytics

--- src/components/backstage/BackstageManageNote.vue
+++ src/components/backstage/BackstageManageNote.vue
@@ -1,9 +1,23 @@
 <script setup>
+    import { ref } from 'vue';
+    const classificationSelected = ref("All")
 
+    function changeClassification() {
+        console.log(classificationSelected.value);
+    }
 </script>
 
 <template>
-    <div>
-        BackstageManageNote
+    <div class="backstage noteManage component">
+        <div class="toolBar">
+            <select name="classification" v-model="classificationSelected" @change="changeClassification">
+                <option value="All">All</option>
+                <option value="Front-end">Front-end</option>
+                <option value="Back-end">Back-end</option>
+                <option value="Algorithm">Algorithm</option>
+                <option value="Database">Database</option>
+                <option value="Game">Game</option>
+            </select>
+        </div>
     </div>
 </template>
